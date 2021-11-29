025f3bfa22204ec2e6e48c91b12a4603425ffc1a
unknown <paomil999@gmail.com>
2021-11-29 18:35:45 -0500

    Se capturan errores en los resolvers y se genera mensaje

--- graphql/auth/resolvers.js
+++ graphql/auth/resolvers.js
@@ -5,42 +5,51 @@ import { generateToken } from "../../utils/tokenUtils.js";
 const resolversAutenticacion = {
     Mutation: {
         registro: async (parent, args) => {
-            const salt = await bcrypt.genSalt(10);
-            const hashedPassword = await bcrypt.hash(args.password, salt);
-            const usuarioCreado = await UsuarioModel.create({
-                nombre: args.nombre,
-                apellido: args.apellido,
-                identificacion: args.identificacion,
-                correo: args.correo,
-                rol: args.rol,
-                password: hashedPassword,
-            });
-            console.log('usuario creado', usuarioCreado);
-            return {
-                token:generateToken({
-                    _id:usuarioCreado._id,
-                    nombre: usuarioCreado.nombre,
-                    apellido: usuarioCreado.apellido,
-                    identificacion: usuarioCreado.identificacion,
-                    correo: usuarioCreado.correo,
-                    rol: usuarioCreado.rol,
-                }),
-            };
+            try {
+                const salt = await bcrypt.genSalt(10);
+                const hashedPassword = await bcrypt.hash(args.password, salt);
+                const usuarioCreado = await UsuarioModel.create({
+                    nombre: args.nombre,
+                    apellido: args.apellido,
+                    identificacion: args.identificacion,
+                    correo: args.correo,
+                    rol: args.rol,
+                    password: hashedPassword,
+                });
+                console.log('usuario creado', usuarioCreado);
+                return {
+                    token:generateToken({
+                        _id:usuarioCreado._id,
+                        nombre: usuarioCreado.nombre,
+                        apellido: usuarioCreado.apellido,
+                        identificacion: usuarioCreado.identificacion,
+                        correo: usuarioCreado.correo,
+                        rol: usuarioCreado.rol,
+                    }),
+                };
+            } catch (error) {
+                return { error: 'Verifique sus datos por favor'}
+            }
         },
 
         login: async (parent, args) => {
-            const usuarioEncontrado = await UsuarioModel.findOne({ correo: args.correo });
-            if (await bcrypt.compare(args.password, usuarioEncontrado.password)) {
-                return {
-                    token:generateToken({
-                        _id:usuarioEncontrado._id,
-                        nombre: usuarioEncontrado.nombre,
-                        apellido: usuarioEncontrado.apellido,
-                        identificacion: usuarioEncontrado.identificacion,
-                        correo: usuarioEncontrado.correo,
-                        rol: usuarioEncontrado.rol,
-                    }),
-                };
+            try {
+                const usuarioEncontrado = await UsuarioModel.findOne({ correo: args.correo });
+                if (await bcrypt.compare(args.password, usuarioEncontrado.password)) {
+                    return {
+                        token:generateToken({
+                            _id:usuarioEncontrado._id,
+                            nombre: usuarioEncontrado.nombre,
+                            apellido: usuarioEncontrado.apellido,
+                            identificacion: usuarioEncontrado.identificacion,
+                            correo: usuarioEncontrado.correo,
+                            rol: usuarioEncontrado.rol,
+                        }),
+                    };
+                }
+
+            } catch (error) {           
+                return {error: 'Correo o contraseña inválidos'}
             }
         },
 
