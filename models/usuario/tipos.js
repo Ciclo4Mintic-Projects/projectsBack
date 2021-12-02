bf5da27dcd67a44def43f26bd52b756fbbb2d66f
unknown <paomil999@gmail.com>
2021-12-01 20:27:55 -0500

    Se agregan resolvers y tipos en usuario para la edición del perfil

--- models/usuario/tipos.js
+++ models/usuario/tipos.js
@@ -1,6 +1,11 @@
 import { gql } from 'apollo-server-express';
 
 const tiposUsuario = gql`
+
+  type Token {
+    token:String
+    error:String
+  }
   
   type Usuario {
     _id: ID!
@@ -37,6 +42,15 @@ const tiposUsuario = gql`
       rol: Enum_Rol
       estado: Enum_EstadoUsuario
     ): Usuario
+
+    editarPerfil(
+      _id: String!
+      nombre: String
+      apellido: String
+      identificacion: String
+      correo: String
+      rol: Enum_Rol
+    ): Token
 
     eliminarUsuario(_id: String, correo: String): Usuario
     
